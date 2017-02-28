3fc3f25afd1253177a9f007ed0238a6c8349d819
Tristan King <tristan@bakkenbaeck.no>
2017-02-28 14:23:14 +0100

    fix error in main

--- tokenrep/app.py
+++ tokenrep/app.py
@@ -32,6 +32,6 @@ class Application(asyncbb.web.Application):
 
 def main():
     app = asyncbb.web.Application(urls)
-    conn = redis.from_url(app.config['redis'].REDIS_URL)
+    conn = redis.from_url(app.config['redis']['url'])
     app.q = Queue(connection=conn)
     app.start()
